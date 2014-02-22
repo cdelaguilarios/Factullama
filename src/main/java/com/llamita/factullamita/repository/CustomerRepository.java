a86d65bbf92781680b17435cfb56946bf04a9d12
aomine12 <aomine.12.6@gmail.com>
2014-02-22 11:54:30 -0500

    Actualizar CustomerRepository

--- src/main/java/com/llamita/factullamita/repository/CustomerRepository.java
+++ src/main/java/com/llamita/factullamita/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.llamita.factullamita.repository;
 
 import java.util.List;
 
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import com.llamita.factullamita.model.Customer;
@@ -11,6 +12,21 @@ public class CustomerRepository extends HibernateRepository{
 
 	public List<Customer> listCustomer(){
 		return null;
+	}
+	
+	public void addCustomer(Customer customer){
+		Transaction tx = null;
+		try{
+			tx = (Transaction) getSession().beginTransaction();
+			getSession().saveOrUpdate(customer);
+			tx.commit();
+		}catch(Exception e){
+			try{
+    			tx.rollback();
+    		}catch(RuntimeException rbe){
+    			System.out.println("Couldn’t roll back transaction "+ rbe);
+    		}
+		}
 	}
 	
 }
