374a8d030bd59a9ac0587a28b2aaa379cf2301d5
aomine12 <aomine.12.6@gmail.com>
2014-02-22 11:08:51 -0500

    init project

--- src/main/java/com/llamita/factullamita/controller/MainController.java
+++ src/main/java/com/llamita/factullamita/controller/MainController.java
@@ -1,5 +1,18 @@
 package com.llamita.factullamita.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
 public class MainController {
 
+	@RequestMapping(value="/index", method= RequestMethod.GET)
+    public String dashboard(ModelMap modelMap) {
+
+        return "index";
+    }
+	
 }
