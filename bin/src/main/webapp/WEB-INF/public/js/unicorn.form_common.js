01e356c01b145e23ae02efb9c3003c4b8cb8e151
JohnPortella <joportm@gmail.com>
2014-03-15 02:26:56 -0500

    Correccion Login - Icono

---
+++ bin/src/main/webapp/WEB-INF/public/js/unicorn.form_common.js
@@ -0,0 +1,12 @@
+/**
+ * Unicorn Admin Template
+ * Version 2.0.1
+ * Diablo9983 -> diablo9983@gmail.com
+**/
+
+$(document).ready(function(){
+
+	$('select').select2();
+    $('.colorpicker').colorpicker();
+    $('.datepicker').datepicker();
+});
